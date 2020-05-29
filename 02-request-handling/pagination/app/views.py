cc23f9b1cdfb3bef1604b86332dc07a14f249cfe
lapssh <lapssh@yandex.ru>
2020-05-29 14:18:59 +0300

    DJ-10 HW-requsts Task_2 Paginator complite

--- 02-request-handling/pagination/app/views.py
+++ 02-request-handling/pagination/app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 import csv
 from django.conf import settings
+from urllib import parse
 
 
 def index(request):
@@ -12,7 +13,7 @@ def index(request):
 def bus_stations(request):
     bus_st = []
     with open(settings.BUS_STATION_CSV, 'r', encoding='cp1251') as csvfile:
-        reader  = csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile)
         for row in reader:
             bus_st.append(row)
 
@@ -21,21 +22,22 @@ def bus_stations(request):
     page_object = paginator.get_page(page)
 
     current_page = page
-    next_page_url = '?page='+str(int(page)+1)
+    print('PAGE:', type(page), page)
+    params_next = {'page': int(page) + 1}
+    params_prev = {'page': int(page) - 1}
+    next_page_url = 'bus_stations?' + parse.urlencode(params_next)
     if int(current_page) > 1:
-        prev_page_url = '?page=' + str(int(page) - 1)
+        prev_page_url = 'bus_stations?' + parse.urlencode(params_prev)
     else:
         prev_page_url = None
+    # next_page_url = '?page='+str(int(page)+1)
+    # if int(current_page) > 1:
+    #     prev_page_url = '?page=' + str(int(page) - 1)
+    # else:
+    #     prev_page_url = None
     return render_to_response('index.html', context={
         'bus_stations': page_object,
         'current_page': current_page,
         'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
-    # return render_to_response('index.html', context={
-    #     'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-    #     'current_page': current_page,
-    #     'prev_page_url': None,
-    #     'next_page_url': next_page_url,
-    # })
-
