99e4036f0926fefe4bf39db3cc501ff8739a4808
lapssh <lapssh@yandex.ru>
2020-05-29 10:31:28 +0300

    DJ-10 HW-requsts Task_2 Paginator is working

--- 02-request-handling/pagination/app/views.py
+++ 02-request-handling/pagination/app/views.py
@@ -1,5 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+import csv
+from django.conf import settings
 
 
 def index(request):
@@ -7,12 +10,32 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    bus_st = []
+    with open(settings.BUS_STATION_CSV, 'r', encoding='cp1251') as csvfile:
+        reader  = csv.DictReader(csvfile)
+        for row in reader:
+            bus_st.append(row)
+
+    paginator = Paginator(bus_st, 10)
+    page = request.GET.get('page', 1)
+    page_object = paginator.get_page(page)
+
+    current_page = page
+    next_page_url = '?page='+str(int(page)+1)
+    if int(current_page) > 1:
+        prev_page_url = '?page=' + str(int(page) - 1)
+    else:
+        prev_page_url = None
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
+        'bus_stations': page_object,
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
+    # return render_to_response('index.html', context={
+    #     'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
+    #     'current_page': current_page,
+    #     'prev_page_url': None,
+    #     'next_page_url': next_page_url,
+    # })
 
