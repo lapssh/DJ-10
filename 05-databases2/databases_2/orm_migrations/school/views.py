cfe04831f532915e40caa6bcb4f140e9b130a0b5
lapssh <lapssh@yandex.ru>
2020-06-17 19:22:03 +0300

    DJ-10 HW-05-BD2  Task2  - Done

--- 05-databases2/databases_2/orm_migrations/school/views.py
+++ 05-databases2/databases_2/orm_migrations/school/views.py
@@ -1,4 +1,3 @@
-from django.views.generic import ListView
 from django.shortcuts import render
 
 from .models import Student
@@ -6,15 +5,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
-    for i in object_list:
-        print(i, i.teacher, i.group)
-    context = {
-        'object_list': object_list
-    }
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    students_list = Student.objects.all().prefetch_related('teacher').order_by(ordering)
 
+    context = {
+        'object_list': students_list
+    }
     return render(request, template, context)
