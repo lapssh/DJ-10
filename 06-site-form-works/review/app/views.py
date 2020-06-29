f3b4bee0a44b5b6bea9edc69bc1d125712effd21
lapssh <lapssh@yandex.ru>
2020-06-29 15:27:35 +0300

    DJ-10 HW-06-Car-admin  Task2  - complite

--- 06-site-form-works/review/app/views.py
+++ 06-site-form-works/review/app/views.py
@@ -19,15 +19,28 @@ def product_list_view(request):
 def product_view(request, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
+    reviews = Review.objects.filter(product=product).order_by('-id')
 
-    form = ReviewForm
+    if 'reviewed_products' not in request.session.keys():
+        request.session['reviewed_products'] = []
+
+    if request.method == 'GET':
+        form = ReviewForm
+
     if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+        form = ReviewForm(request.POST)
+        text_ = ReviewForm(request.POST)['text'].value()
+        Review.objects.create(text=text_, product=product)
+        request.session['reviewed_products'].append(product.id)
+        request.session.modified = True
 
     context = {
         'form': form,
-        'product': product
+        'product': product,
+        'reviews': reviews
     }
+
+    if product.id in request.session['reviewed_products']:
+        context['is_review_exist'] = True
 
     return render(request, template, context)
